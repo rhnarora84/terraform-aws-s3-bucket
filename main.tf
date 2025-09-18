8cea66164f080ad78c973adc461ba4a0c6ca9da1
rhnarora84 <rhnarora84@gmail.com>
2025-09-18 14:09:45 +0530

    adding main.tf file

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "eu-central-1"
+}
+
+provider "random" {
+  # Configuration options for the random provider
+  
+}
+
+resource "random_string" "demorandomstring" {
+    length = 8
+    upper = false
+    special = false
+  
+}
+
+resource "aws_s3_bucket" "random_bucket" {
+  bucket = "rohanarora-${random_string.demorandomstring.result}"
+}
